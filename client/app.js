8eb3ea896ca9942c2a37c3e9f2da9aa5f566141a
Peter Visser <pjdvisser@gmail.com>
2016-12-21 16:07:02 +0100

    no message

--- client/app.js
+++ client/app.js
@@ -34,7 +34,7 @@ var main = function () {
                 
                 
             if (currentTodo.done === "false") {
-            $(".container .todoList .tasks ul").prepend(listItem.append("<input type="submit" value="update">"));
+            $(".container .todoList .tasks ul").prepend(listItem.append($("<input type="submit" value="update">")));
             } else if (currentTodo.done === "true") {
             $(".container .todoList .done  ul").prepend(listItem);
             }
